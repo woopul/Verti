bd5af02e3c8e0209a4860a5bb4ce4c2b40e2b66d
Wojtek Opulski <wojtekopulski@gmail.com>
2018-09-14 12:32:21 +0200

    style changes, media correction

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 var mobile = window.matchMedia("screen and (max-width: 760px)");
-var menu = document.querySelector(".menu");
-var menuTrigger = document.getElementById("menu-trigger");
+var menu = $(".menu");
+var menuTrigger = $("#menu-trigger");
 
 
 if (mobile.matches) {
-    menuTrigger.style.display = "block";
+    $(menuTrigger).css('display' , 'block');
 } else {
-    menuTrigger.style.display = 'none';
+    $(menuTrigger).css('display' , 'none');
 }
-mobile.addListener(function () {
-    if (mobile.matches) {
-        menuTrigger.style.display = "block";
-    } else {
-        menuTrigger.style.display = 'none';
-    }
-});
+// mobile.addListener(function () {
+//     if (mobile.matches) {
+//         menuTrigger.css('display' , 'block');
+//     } else {
+//         menuTrigger.css('display' , 'none');
+//     }
+// });
 
 menuTrigger.addEventListener("click", function () {
     if(menu.style.display === "block"){
@@ -22,5 +22,4 @@ menuTrigger.addEventListener("click", function () {
     } else {
         menu.style.display = "block";
     }
-
-})
+});
